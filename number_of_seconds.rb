fa8fad0e1865a01d6c5efed4bcab2fa45853f116
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-02-19 22:58:00 -0700

    starting to write the script per homework direction [#114113795]

---
+++ number_of_seconds.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+seconds_in_a_minute = 60
+minutes_in_an_hour = 60
+hours_in_a_day = 24
+days_in_a_week = 7
+
+puts "There are #{seconds_in_a_minute} seconds in a minute."
+puts "There are #{minutes_in_an_hour} minutes in an hour."
+puts "There are #{hours_in_a_day} hours in a day."
+puts "There are #{days_in_a_week} days in a week."
+puts "That means there are:
+puts  #{seconds_in_a_minute *minutes_in_an_hour} "seconds in an hour,"
