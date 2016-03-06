ee5a7398e66581acc913b057d340734ba92be7bd
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-03-05 17:08:01 -0700

    Editing format for rubocop and travis errors [#114113795]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -1,22 +1,23 @@
 #!/usr/bin/env ruby
 
-seconds_in_a_minute = 60
-minutes_in_an_hour = 60
-hours_in_a_day = 24
-days_in_a_week = 7
+seconds_per_minute = 60
+minutes_per_hour = 60
+hours_per_day = 24
+days_per_week = 7
+days_per_year = 365
+seconds_per_hour = seconds_per_minute * minutes_per_hour
+seconds_per_day = seconds_per_hour * hours_per_day
+seconds_per_year = seconds_per_day * days_per_year
 
-puts "There are #{seconds_in_a_minute} seconds in a minute."
-puts "There are #{minutes_in_an_hour} minutes in an hour."
-puts "There are #{hours_in_a_day} hours in a day."
-puts "There are #{days_in_a_week} days in a week."
-puts "That means there are:"
-puts "#{seconds_in_a_minute * minutes_in_an_hour} seconds in an hour,"
-puts "#{seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day} seconds in an day,"
-puts "#{seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day * days_in_a_week} seconds in a week"
-puts "That means when you turn 20, you've been alive for\
-  #{seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day * days_in_a_week * 52 * 20} seconds,"
-puts "and if you make it too 100, you will have lived\
-  #{seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day * days_in_a_week * 52 * 100} seconds. Make them count!"
-
-
-
+puts "There are #{seconds_per_minute} seconds in a minute"
+puts "There are #{minutes_per_hour} minutes in an hour"
+puts "There are #{hours_per_day} hours in a day"
+puts "There are #{days_per_week} days in a week"
+puts 'That means there are:'
+puts "  #{seconds_per_hour} seconds in an hour,"
+puts "  #{seconds_per_day} seconds in a day,"
+puts "  #{seconds_per_day * days_per_week} seconds in a week"
+print "That means when you turn 20, you've been alive for "
+puts "#{20 * seconds_per_year} seconds,"
+print 'and if you make it to 100, you will have lived '
+puts "#{100 * seconds_per_year} seconds. Make them count!"
