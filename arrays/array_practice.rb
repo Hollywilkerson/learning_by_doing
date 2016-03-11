b6d6f797b3c5061775d8840b2af0be6dd217a4ed
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-03-10 19:06:01 -0700

    Changing format for rubocop offenses [#114688049]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -1,21 +1,24 @@
-#!/usr/bin env ruby
+#!/usr/bin/env ruby
 
-array = (1..10).to_a
+starting_array = (1..10).to_a
 
-puts "#{array.join('...')}..."
-puts "T-#{array.reverse.join(', ')}...  BLASTOFF!"
-puts "The last element is #{array.last}"
-puts "The first element is #{array.first}"
-puts "The third element is #{array[2]}"
-puts "The element with an index of 3 is #{array[3]}"
-puts "The second from last element is #{array[-2]}"
-puts "The first four elements are '#{array[0..3].join(', ')}'"
-puts "If we delete 5, 6 and 7 from the array, we're left with " \
-     "[#{array.delete_if { |num| num > 4 && num < 8 }.join(',')}]"
-puts "If we add 5 at the beginning of the array, we're left with " \
-     "[#{array.insert(0, 5).join(',')}]"
-puts "If we add 6 at the end of the array, we're left with " \
-     "[#{array.push(6).join(',')}]"
-puts "Only the elements #{array.select { |num| num > 8 }} are > 8."
-puts 'If we remove all the elements, then the length of the array is ' \
-     "#{array.clear.length}"
+puts "#{starting_array.join('...')}..."
+puts "T-#{starting_array.reverse.join(', ')}...  BLASTOFF!"
+puts "The last element is #{starting_array.last}"
+puts "The first element is #{starting_array.first}"
+puts "The third element is #{starting_array[2]}"
+puts "The element with an index of 3 is #{starting_array[3]}"
+puts "The second from last element is #{starting_array[-2]}"
+puts "The first four elements are '#{starting_array.first(4).join(', ')}'"
+starting_array.delete_if { |digit| [5, 6, 7].include?(digit) }
+print 'If we delete 5, 6 and 7 from the array,'
+puts " we're left with [#{starting_array.join(',')}]"
+starting_array.unshift(5)
+print 'If we add 5 at the beginning of the array,'
+puts " we're left with [#{starting_array.join(',')}] "
+starting_array.push(6)
+print 'If we add 6 at the end of the array,'
+puts " we're left with [#{starting_array.join(',')}]"
+puts "Only the elements #{starting_array.select { |digit| digit > 8 }} are > 8."
+print 'If we remove all the elements,'
+puts " then the length of the array is #{starting_array.clear.length}"
