261eca2c1e173d535cb9d3bcce0361508c8a519f
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-03-24 11:46:09 -0600

    Making changes to object remote, showing how it works per homework [#114692975]

--- objects/remote_control.rb
+++ objects/remote_control.rb
@@ -1,31 +1,40 @@
 # This is a RemoteControl Class
 class RemoteControl
-  def initialize(functions)
-    @functions = functions
+  def initialize(volume)
+    @volume = volume
   end
 
   def volume_up(number)
-    @functions = number + 1
+    @volume = number + 1
   end
 
   def volume_down(number)
-    @functions = number - 1
+    @volume = number - 1
+  end
 
-    def mute(silent)
-      @functions = silent
-    end
-
+  def mute(number)
+    @volume = number == 0
   end
 
   def channel_up(number)
-    @functions = number + 1
+    @volume = number + 1
   end
 
-  def channel_Down(number)
-    @functions = number - 1
+  def channel_down(number)
+    @volume = number - 1
   end
 
-  def menu(options)
-    @functions = options
+  def menu
+    @menu = [Contrast, Brightness, Sharpness]
+    menu.each do |menu|
   end
-end
+  end
+
+remote_control = RemoteControl.new(4)
+puts remote_control.volume_up(3)
+puts remote_control.volume_down(5)
+puts remote_control.mute(0)
+puts remote_control.channel_up(10)
+puts remote_control.channel_down(15)
+puts remote_control.menu
+
