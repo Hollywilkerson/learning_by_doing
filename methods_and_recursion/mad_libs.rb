37724c4087b8b77a5e9c7517d353ce66815813f5
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-03-23 21:33:41 -0600

    fixing rubocop errors for file and making executable [#114692325]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -25,22 +25,19 @@ var17 = ask_question 'Give me another adjective.'
 var18 = ask_question 'Give me a number.'
 var19 = ask_question 'Give me another plural noun. '
 
-def mad_lib
-  puts 'A vacation is when you take a trip to some #{var1} place with your #{var2} family.'
-  puts 'Usually you go to some place that is near a/an #{var2} or up on a/an #{var3}.'
-  puts 'A good vacation place is one where you can ride #{var4} or play #{var4} or ' \
-    +  'go hunting for #{var5}.'
-  puts 'I like to spend my time #{var6} or #{var7}.'
-  puts 'When parents go on a vacation, they spend their time eating three #{var8}'  \
-    +  ' a day and fathers play golf, and mothers sit around #{var9}.'
-  puts 'Last summer, my little brother fell in a/an #{var10} and got poison #{var11},' \
-    +  'all over his #{var12}.'
-  puts 'My family is going to go to (the) #{var13}, and I will practice #{var14}.'
-  puts 'Parents need vacations more than kids because parents are always very #{var15}' \
-    + 'and because they have to work #{var16} hours every day all year making enough' \
-    + '#{var16} to pay for the vacation.'
-end
+mad_lib = <<mad_lib
+  puts "A vacation is when you take a trip to some #{var1} place with your #{var2} family."
+  puts "Usually you go to some place that is near a/an #{var2} or up on a/an #{var3}."
+  puts "A good vacation place is one where you can ride #{var4} or play #{var4} or " \
+    +  "go hunting for #{var5}."
+  puts "I like to spend my time #{var6} or #{var7}."
+  puts "When parents go on a vacation, they spend their time eating three #{var8}"  \
+    +  " a day and fathers play golf, and mothers sit around #{var9}."
+  puts "Last summer, my little brother fell in a/an #{var10} and got poison #{var11}," \
+    +  "all over his #{var12}."
+  puts "My family is going to go to (the) #{var13}, and I will practice #{var14}."
+  puts "Parents need vacations more than kids because parents are always very #{var15}" \
+    + "and because they have to work #{var16} hours every day all year making enough" \
+    + "#{var16} to pay for the vacation."
+mad_lib
 puts mad_lib
-
-
-
