e9344ab6a7938c4a96e1ccd045233820a0c10fba
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-03-24 14:16:22 -0600

    Editing homework per pivital tracker notes [#114692975]

--- objects/remote_control.rb
+++ objects/remote_control.rb
@@ -4,41 +4,58 @@
 class RemoteControl
   attr_reader :volume
 
-  def initialize
-    @volume = 0 +rand(60)
+  def initialize()
+    @volume = 50
+    @mute = false
+    @channel = 192
   end
 
-  def volume_up(number)
-    @volume = number + 1
+  def volume_up()
+    @volume = @volume + 1
   end
 
-  def volume_down(number)
-    @volume = number - 1
+  def volume_down()
+    @volume =  @volume - 1
   end
 
-  def mute(number)
-    @volume = number == 0
+  def mute(mute)  //toggle - on/off
+    if mute == true
+      @mute = true
+    else
+      @mute = false
+    end
   end
 
-  def channel_up(number)
-    @volume = number + 1
+  def channel_up()
+    @channel = @channel + 1
   end
 
-  def channel_down(number)
-    @volume = number - 1
+  def channel_down()
+    @channel = @channel - 1
   end
 
-  def menu
-    @menu = [Contrast, Brightness, Sharpness]
+  def select_channel(number)
+    @channel = number
+  end
+
+  def show_channel()
+    puts @channel
+  end
+
+  def menu()
+    @menu = %w (contrast brightness sharpness)
     menu.each do |menu|
     end
   end
 
-remote_control = RemoteControl.new(4)
-puts remote_control.volume_up(3)
-puts remote_control.volume_down(5)
-puts remote_control.mute(0)
-puts remote_control.channel_up(10)
-puts remote_control.channel_down(15)
-puts remote_control.menu
-
+remote_control = RemoteControl.new()
+remote_control.volume_up()  //raises volume by 1
+remote_control.volume_down()
+remote_control.mute(true)  // enables mute
+remote_control.channel_up()
+remote_control.channel_down()
+remote_control.select_channel(23)  //selects channel 23
+remote_control.show_channel()  // will return 23
+remote_control.select_channel(11)
+remote_control.show_channel()  // will return 11
+remote_control.menu
