aa23517d8950992e092098bc427a99555b4cdcb8
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-16 13:25:05 -0600

    fixing format for travis [#114692325]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -25,22 +25,23 @@ var17 = ask_question 'Give me another adjective.'
 var18 = ask_question 'Give me a number.'
 var19 = ask_question 'Give me another plural noun. '
 
-mad_lib = <<mad_lib
-A vacation is when you take a trip to some #{var1} place/
-with your #{var2} family.
-Usually you go to some place/that is near a/an #{var3}/
-or up on a/an #{var4}.
-A good vacation place is one where you can ride #{var5} or play #{var6} or  \
-go hunting for #{var7}.
-I like to spend my time #{var8} or #{var9}.
-When parents go on a vacation, they spend their time eating three #{var10}  \
-a day and fathers play golf, and mothers sit around #{var11}.
-Last summer, my little brother fell in a/an #{var12} and got poison #{var13}, \
-all over his #{var14}.
-My family is going to go to (the) #{var15}, and I will practice #{var16}.
-Parents need vacations more than kids because parents are always very #{var17} \
-and because they have to work #{var18} hours every day all year making enough \
-#{var19} to pay for the vacation.
-mad_lib
+mad_lib = <<MAD_LIB
+A vacation is when you take a trip to some #{var1} place
+with your #{var2} family. Usually you go to some place
+that is near a/an #{var3} or up on a/an #{var4}.
+A good vacation place is one where you can ride #{var5}
+or play #{var6} or go hunting for #{var7}. I like
+to spend my time #{var8} or #{var9}.
+When parents go on a vacation, they spend their time eating
+three #{var10} a day, and fathers play golf, and mothers
+sit around #{var11}. Last summer, my little brother
+fell in a/an #{var12} and got poison #{var13} all
+over his #{var14}. My family is going to go to (the)
+#{var15}, and I will practice #{var16}. Parents
+need vacations more than kids because parents are always very
+#{var17} and because they have to work #{var18}
+hours every day all year making enough #{var19} to pay
+for the vacation.
+MAD_LIB
 
 puts mad_lib
